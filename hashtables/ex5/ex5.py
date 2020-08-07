2600f960ddd776092a08fedaf2525794e62460fe
David Horstman <davidzilla27@gmail.com>
2020-08-07 11:48:49 -0400

    Ex 5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,5 +1,8 @@
 # Your code here
-
+class Entry:
+    def __init__(self,path,next=None):
+        self.path = path
+        self.next = next
 
 
 def finder(files, queries):
@@ -7,7 +10,22 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-
+    path_dict = {}
+    result = []
+    # Populate dict
+    for path in files:
+        filename = path.split("/")[-1]
+        old_head = path_dict[filename] if path_dict.get(filename) else None
+        path_dict[filename] = Entry(path,old_head)
+    
+    # Search through dict
+    for filename in queries:
+        if not path_dict.get(filename):
+            continue
+        curr_path = path_dict[filename]
+        while curr_path is not None:
+            result.append(curr_path.path)
+            curr_path = curr_path.next
     return result
 
 
